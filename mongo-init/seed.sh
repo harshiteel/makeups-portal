45fb176b7e24c799098fc821fd21f42877a5d33c
Harshit Agrawal <f20230480@hyderabad.bits-pilani.ac.in>
2025-09-21 15:06:34 +0530

    Fixed issues with dockerization and removed unnecessary files

--- mongo-init/seed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script will be run automatically by the mongo container on its first startup.
-# It restores the database from the backup we mounted at /mongo-seed.
-
-echo "🌱 Seeding database with initial data..."
-mongorestore --db ID-makeups /mongo-seed/ID-makeups
-echo "✅ Database seeded."
-
